fc57839154175feca507bcfcfb9b2fa22e578e2a
DAILF28 <floresdd28@gmail.com>
2026-07-03 16:27:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const musica = {
     titulo: "Nuestra canción",
-    enlace: "https://open.spotify.com/"
+    enlace: "https://open.spotify.com/intl-es/track/2VF8TTWczzupwrhtsQfx48?si=40a384de7e0547ee"
 };
